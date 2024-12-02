98c8188c3d5d825cbaf365cc0fbd68fc1ce99c93
Johanssono <113384810+Johanssono@users.noreply.github.com>
2024-12-02 21:10:35 +0100

    testa om klart

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,33 +1,82 @@
-const cacheName = "v1";
+import EventManager from "./EventManager.js";
 
-const cacheAssets = [
-  "index.html",
-  "/css/main.css",
-  "/js/main.js"
-]
-
-
+class ServiceWorker {
+    
+    constructor(){
+        this.eventManager = new EventManager();
+    }
 
 
+    async AddResourcesToCache(resources){
+        const cache = await cache.open("site cashe");
+        await cache.addAll(resources);
+    }
+
+    InstallationEvent(event){
+      caches.open(staticCacheName).then(cache => {
+        console.log('caching shell assets');
+        cache.addAll(
+          '/',
+          '/index.html',
+          '/css/main.css',
+          '/icon512_maskable.png',
+        );
+    })
+    }
+
+    ActiveEvent(oklart) {
+      console.log('service woker has been activated');
+    }
+
+    
+    FetchEvent(fortfarandeoklart) {
+      console.log('fetch event', evt);
+      evt.respondWith(
+          caches.match(evt.request).then(cacheRes => {
+              return cacheRes || fetch(evt.request);
+          })
+      );
+    }
 
 
-self.addEventListener("install", e => {
-  console.log("sug den kenneth");
 
-  e.waituntil (
-    cache
-      .open(cacheName)
-      .then(cache => {
-        console.log("kuksugare");
-        cache.addAll(cacheAssets);
-      })
-      .then(() => self.skipwaiting())
-  )
-});
 
-self.addEventListener("install", e => {
-  console.log("jävla skit")
-})
+    
+  Main(){
+    const installationEvent = "install";
+    const addResourcesToCache = "cache";
+    const activeevent = "active";
+    const fetchevent = "fetch";
+    eventManager.NoneEementEventListener(installationEvent, this.InstallationEvent);
+    eventManager.NoneEementEventListener(addResourcesToCache, this.AddResourcesToCache);
+    eventManager.NoneEementEventListener(activeevent, this.ActiveEvent);
+    eventManager.NoneEementEventListener(fetchevent, this.FetchEvent);
+
+  }
+
+
+}
+
+    
+const serviceworker = new Serviceworker();
+
+serviceworker.Main();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
