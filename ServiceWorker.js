014bde087487d6b57b73b93d4dfebc73ae7b5e16
Johanssono <113384810+Johanssono@users.noreply.github.com>
2024-12-03 16:48:10 +0100

    efsefAWEFAWDFasefAWFAWFAWFAWFwsfsfASF

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,16 +1,70 @@
+const staticCacheName = 'site-static';
+const assets = [
+    '/',
+    '/index.html',
+
+
+
+];
+
+self.addEventListener('install', evt => {
+    console.log('service worker has been installed');
+    evt.waitUntil(    
+        caches.open(staticCacheName).then(cache => {
+            console.log('caching shell assets');
+            cache.addAll(assets);
+        })
+    );
+});
+
+
+
+self.addEventListener('activate', evt => {
+    console.log('service woker has been activated');
+});
+
+
+
+self.addEventListener('fetch', evt => {
+    console.log('fetch event', evt);
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request);
+        })
+    );
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 import EventManager from "./EventManager.js";
 
 class ServiceWorker {
     
     constructor(){
         this.eventManager = new EventManager();
-    }
+    };
 
 
     async AddResourcesToCache(resources){
         const cache = await cache.open("site cashe");
         await cache.addAll(resources);
-    }
+    };
 
     InstallationEvent(event){
       caches.open(staticCacheName).then(cache => {
@@ -20,12 +74,12 @@ class ServiceWorker {
           '/index.html',
 
         );
-    })
-    }
+    });
+    };
 
     ActiveEvent(oklart) {
       console.log('service woker has been activated');
-    }
+    };
 
     
     FetchEvent(fortfarandeoklart) {
@@ -35,7 +89,7 @@ class ServiceWorker {
               return cacheRes || fetch(evt.request);
           })
       );
-    }
+    };
 
 
 
@@ -51,12 +105,14 @@ class ServiceWorker {
     eventManager.NoneEementEventListener(activeevent, this.ActiveEvent);
     eventManager.NoneEementEventListener(fetchevent, this.FetchEvent);
 
-  }
+  };
 
 
-}
+};
 
     
 const serviceworker = new Serviceworker();
 
 serviceworker.Main();
+
+*/
