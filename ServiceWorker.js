590456aefd5076074304f60b17c5ae6feb20155d
Johanssono <113384810+Johanssono@users.noreply.github.com>
2024-12-03 16:02:12 +0100

    skiiit

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,3 +1,49 @@
+const staticCacheName = 'site-static';
+const assets = [
+  '/',
+  '/index.html',
+  '/css/main.css',
+  '/icon512_maskable.png',
+
+
+];
+
+self.addEventListener('install', evt => {
+    console.log('service worker has been installed');
+    evt.waitUntil(    
+        caches.open(staticCacheName).then(cache => {
+            console.log('caching shell assets');
+            cache.addAll(assets);
+        })
+    );
+});
+
+
+
+self.addEventListener('activate', evt => {
+    console.log('service woker has been activated');
+});
+
+
+
+self.addEventListener('fetch', evt => {
+    console.log('fetch event', evt);
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request);
+        })
+    );
+});
+
+
+
+
+
+
+
+
+/*
+
 import EventManager from "./EventManager.js";
 
 class ServiceWorker {
@@ -92,7 +138,6 @@ serviceworker.Main();
 
 
 
-/*
 
 import EventManager from "./EventManager.js";
 
