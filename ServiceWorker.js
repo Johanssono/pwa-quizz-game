c823c6c0f6dd7b376bc69e560cac5fe80b272846
Johanssono <113384810+Johanssono@users.noreply.github.com>
2024-12-03 17:15:28 +0100

    för

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,3 +1,38 @@
+const staticCacheName = 'site-static';
+const assets = [
+    '/',
+    '/index.html',
+];
+
+self.addEventListener('install', evt => {
+    console.log('service worker has been installed');
+    evt.waitUntil(    
+        caches.open(staticCacheName).then(cache => {
+            console.log('caching assets');
+            cache.addAll(assets);
+        })
+    );
+});
+
+
+
+self.addEventListener('activate', evt => {
+    console.log('service woker has been activated');
+});
+
+
+
+self.addEventListener('fetch', evt => {
+    console.log('fetch event', evt);
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request);
+        })
+    );
+});
+
+/*
+
 import EventManager from "./EventManager.js";
 
 class ServiceWorker {
@@ -60,3 +95,5 @@ class ServiceWorker {
 const serviceworker = new Serviceworker();
 
 serviceworker.Main();
+
+*/
