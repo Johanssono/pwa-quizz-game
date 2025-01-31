f3df8de8f298846b66a695e3560952257230ca1c
Johanssono <ottojohanssonjobb@gmail.com>
2025-01-31 09:20:02 +0100

    I

--- js/ServiceWorker.js
+++ js/ServiceWorker.js
@@ -29,12 +29,10 @@ self.addEventListener('activate', evt => {
 self.addEventListener('fetch', evt => {
     console.log('fetch event', evt);
 
-    /*
     evt.respondWith(
         caches.match(evt.request).then(cacheRes => {
             return cacheRes || fetch(evt.request);
         })
     );
 
-    */
 });
