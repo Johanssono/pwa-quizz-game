01cc6070d7540b8e416b070b1dce237fa3cf9e8c
Johanssono <ottojohanssonjobb@gmail.com>
2025-02-07 10:13:14 +0100

    se if sound starts in index.html

--- js/Settings.js
+++ js/Settings.js
@@ -1,8 +1,8 @@
+
 function setCookie(c_name, value, exdays) {
     var exdate = new Date();
     exdate.setDate(exdate.getDate() + exdays);
-    var c_value = escape(value) +
-        ((exdays == null) ? "" : "; expires=" + exdate.toUTCString());
+    var c_value = escape(value) + ((exdays == null) ? "" : "; expires=" + exdate.toUTCString());
     document.cookie = c_name + "=" + c_value;
 }
 
@@ -39,7 +39,6 @@ function update() {
     }
 }
 setInterval(update, 1000);
-
 
 
 
